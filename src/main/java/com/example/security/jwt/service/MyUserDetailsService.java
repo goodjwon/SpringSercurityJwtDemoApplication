50a8d3ba9132882709f12d484d1c2e3474f7ca93
goodjwon <firstman9@protonmail.com>
2022-08-15 12:25:09 +0900

    make jwt

--- src/main/java/com/example/security/jwt/service/MyUserDetailsService.java
+++ src/main/java/com/example/security/jwt/service/MyUserDetailsService.java
@@ -6,20 +6,46 @@ import com.example.security.jwt.repository.RoleRepository;
 import com.example.security.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class MyUserDetailsService implements UserService {
+public class MyUserDetailsService implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        if(user==null){
+            log.error("{} user not found in the database", username);
+            throw new UsernameNotFoundException("user not found in the database");
+        } else {
+            log.info("User found in the database: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getRoleName())));
+
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getUserPwd(),authorities );
+    }
 
     @Override
     public User saveUser(User user) {
         log.info("Saving new user {} to the database", user.getUserName());
+        user.setUserPwd(passwordEncoder.encode(user.getUserPwd()));
+
         return userRepository.save(user);
     }
 
@@ -48,4 +74,6 @@ public class MyUserDetailsService implements UserService {
         log.info("Fetching All users ");
         return userRepository.findAll();
     }
+
+
 }
