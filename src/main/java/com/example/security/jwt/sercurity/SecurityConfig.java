16d4770cd73f9ec88c771252508bdf1cf300a3ac
goodjwon <firstman9@protonmail.com>
2022-08-17 17:16:59 +0900

    added 커스텀 필터

--- src/main/java/com/example/security/jwt/sercurity/SecurityConfig.java
+++ src/main/java/com/example/security/jwt/sercurity/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.security.jwt.sercurity;
 
 
 import com.example.security.jwt.filter.CustomAuthenticationFilter;
+import com.example.security.jwt.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -42,6 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(POST, "/api/users/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
 
