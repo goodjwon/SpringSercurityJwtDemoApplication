61d0a0a2972435177d49e8a4538a856a9d20f4fb
JungWon Park <firstman9@protonmail.com>
2022-08-20 10:39:50 +0900

    컨트롤러 테스트 작성 jwt 셋팅 추가 필요.

---
+++ src/test/java/com/example/controller/UserControllerTest.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @WithMockUser(username = "Eli Jung", password = "1234", roles = {"USER"})
+    public void getUsers() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/users"))
+                .andExpect(status().isOk());
+    }
+}
